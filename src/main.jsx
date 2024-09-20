e963180fe1e43b1d25d0a700b011f7e36eb906bd
Vikash kumar <vikashkumargauravdss@gmail.com>
2024-09-20 09:26:54 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // Import toastify CSS
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    
+    <App />
+    <ToastContainer />
+    
+  </StrictMode>,
+)
